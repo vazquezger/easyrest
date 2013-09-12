5f8be4984c525b76acaa85a2cc2491f7dff1feae
gvazquez-elance <gvazquez@elance.com>
2013-09-12 13:24:48 -0300

    First commit.

---
+++ src/test/java/com/elance/infrastructure/easyrest/GitHubClient.java
@@ -0,0 +1,22 @@
+package com.elance.infrastructure.easyrest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/")
+public interface GitHubClient {
+
+	@GET
+	@Path("/users")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getUsers();
+	
+	@GET
+	@Path("/users/{username}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getUserByUsername(@PathParam("username") String username);
+		
+}
